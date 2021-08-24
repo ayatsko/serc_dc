c3d866481c03830f7a1df36cb56125ac47c80c80
abbeyyatsko <53024069+ayatsko@users.noreply.github.com>
2021-08-24 14:50:14 -0400

    assign samples to gas flux data - pull out samples / recording time ONLY

--- scripts/pilot_LGR_processing.R
+++ scripts/pilot_LGR_processing.R
@@ -3,12 +3,12 @@
 
 # DAY 1 processing ---- 
 # set year,month and date of files being processed
-year='2021'
-month='August'
-date='2021-08-03'
+year <- '2021'
+month <- 'August'
+date <- '2021-08-03'
 
 # identify working directory for corresponding date
-wd=paste('/Users/abbeyyatsko/Desktop/repos/serc_deadwood/pilot_LGR/2021-08-03',
+wd <- paste('/Users/abbeyyatsko/Desktop/repos/serc_deadwood/pilot_LGR/2021-08-03',
          year,'/',month,'/',date,sep='')
 
 # step 1 - format LGR raw data
@@ -21,37 +21,36 @@ setwd("/Users/abbeyyatsko/Desktop/repos/serc_deadwood/pilot_LGR/2021-08-03")
 
 # get names of all LGR files with GHG concentration
 ## data ('...f####.txt')
-filenames=list.files(pattern='f0',full.names=T)
+filenames <- list.files(pattern='f0',full.names=T)
 
 ## Read in LGR datafiles to list
-dat=lapply(filenames,read.csv,skip=1)
-
-## Remove rows with NAs
-# dat=lapply(dat,na.omit)
-# NOTE this caused an error - remove step 
+dat <- lapply(filenames,read.csv,skip=1)
 
 ## Combine all files into single dataframe
-dat.all=do.call(rbind,dat)
+dat.all <- do.call(rbind,dat)
 
 ## Pull out date and time data
-date_time=strptime(dat.all[,1],format='%m/%d/%Y %H:%M:%S')
+date_time <- strptime(dat.all[,1],format='%m/%d/%Y %H:%M:%S')
 
 ## Add year,month,day,JD,hour,min,sec columns to dataframe
-Year=as.numeric(format(date_time,'%Y'))
-Month=as.numeric(format(date_time,'%m'))
-Day=as.numeric(format(date_time,'%d'))
-fDOY=as.numeric(julian(date_time,'2021-01-01'))  #Change for year
-Hour=as.numeric(format(date_time,'%k'))
-Min=as.numeric(format(date_time,'%M'))
-Sec=as.numeric(format(date_time,'%S'))
-dat.all=cbind(date_time,Year,Month,Day,fDOY,Hour,Min,Sec,dat.all[,-1])
+Year <- as.numeric(format(date_time,'%Y'))
+Month <- as.numeric(format(date_time,'%m'))
+Day <- as.numeric(format(date_time,'%d'))
+fDOY <- as.numeric(julian(date_time,'2021-01-01'))  #Change for year
+Hour <- as.numeric(format(date_time,'%k'))
+Min <- as.numeric(format(date_time,'%M'))
+Sec <- as.numeric(format(date_time,'%S'))
+dat.all <- cbind(date_time,Year,Month,Day,fDOY,Hour,Min,Sec,dat.all[,-1])
 
 ## Save LGR data as data.table
-dat.all=data.table(dat.all)
+dat.all <- data.table(dat.all)
 
 # preliminary plots
 # for co2 
 library(scales)
+library(ggplot2)
+library(ggpubr)
+
 co2_d1 <- ggplot(dat.all, aes(date_time, X.CO2.d_ppm)) +
   geom_point() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
@@ -61,23 +60,24 @@ ch4_d1 <- ggplot(dat.all, aes(date_time, X.CH4.d_ppm)) +
   geom_point() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
 
-library(ggarrange)
 ggarrange(co2_d1, ch4_d1, nrow = 2)
 
 # step 2 - format plot metadata
 
 # Read in file with plot data
-plots=read.csv('plot_metadata_day1.csv')
+plots <- read.csv('plot_metadata_day1.csv')
 as.Date(plots$Date, '%m/%d/%y')
 str(plots)
+
 ## Pull out date and time data
 plot_date_time_start=strptime(paste(plots$Date,plots$Time_start),'%m/%d/%y %H:%M')
 
 ## Add 30 seconds to set start time to be within flux period 
 plot_date_time_start=plot_date_time_start+30
 
-## Add 5 min (300 sec) to flux start time to get flux end time
-plot_date_time_end=plot_date_time_start+300
+## Add 5 min (300 sec) - the above 30 sec interlude (total s = 270) to flux start 
+# time to get flux end time
+plot_date_time_end=plot_date_time_start+270
 
 ## Add fDOY columns for start and endtime to dataframe
 fDOY_start=as.numeric(julian(plot_date_time_start,'2021-01-01'))  #Change for year
@@ -90,18 +90,26 @@ plots=cbind(fDOY_start,fDOY_end,plots)
 library(sqldf)
 
 ## merge LGR/log and plot metadata files by time of flux (in fractional DOY)
+# note that this part diverges from Genevieve's initial script - still use SQL 'between' to sort out 
+# when flux measurements were actually being made for samples
 
-## 'inner join' removes all rows that are not during chamber placement
-## 'left join' keeps all data and adds NAs for data not during chamber placement
-dat_merged <- sqldf("select * from dat.all
-                 on (dat_all.fDOY between plots.fDOY_start and plots.fDOY_end)")
+# necessary things to rename so that they will go thru sql much smoother 
+dat_all <- dat.all
+dat_all$ch4 <- dat_all$X.CH4._ppm # just consider methane for now as a preliminary look 
 
-seqdf <- data.frame(thetime=seq(100,225,5),thevalue=factor(letters))
-boundsdf <- data.frame(thestart=c(110,160,200),theend=c(130,180,220),groupID=c(555,666,777))
-
-# run the same query using 'between...and' clause
-testquery_2 <- sqldf("select seqdf.thetime, seqdf.thevalue, boundsdf.groupID 
-from seqdf LEFT JOIN boundsdf ON (seqdf.thetime BETWEEN boundsdf.thestart AND boundsdf.theend)")
-
-myquery_2 <- sqldf("select dat_all.fDOY, dat_all.ch4, plots.Time_start 
+testquery <- sqldf("select dat_all.fDOY, dat_all.ch4, plots.PIECETAG 
 from dat_all LEFT JOIN plots ON (dat_all.fDOY BETWEEN plots.fDOY_start AND plots.fDOY_end)")
+
+# NICE IT WORKS EYYYYYY 
+
+# now we need to parse through only the data recorded for ch4 where PIECETAG is recorded - this means 
+# that the data actually means something and is assigned to an actual sample
+
+# use 'complete.cases'
+testquery <- testquery[complete.cases(testquery), ]
+
+# step 4 - visually check clipped bits of data 
+# ch4 
+par(mfrow=c(2,1),mar=c(4,4,1,1))
+with(dat_all,plot(fDOY,X.CH4.d_ppm,ylim=c(1.95,3)))
+with(testquery,plot(fDOY,ch4,ylim=c(1.95,3)))
